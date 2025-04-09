2e1122c9028c6618dfe8b8b55df54bb4242dc17b
declanraj <rsm.raj@gmail.com>
2025-04-08 22:09:31 -0400

    Update settings to restrict ALLOWED_HOSTS and modify api_root to use base URL

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&^(^palf@ykj^-40qe)!d7t!t^nnl8qh+^6ry5$_2=ks=v6kuo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
